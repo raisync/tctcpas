6a5f2aeb49c89d2bcd655efb54e1a0587822d416
Ryan B. Abcede <ryan@syntactics.local>
2017-10-03 18:58:24 +0800

    blank slate

---
+++ wp-content/themes/tctcpas/footer.php
@@ -0,0 +1,20 @@
+			<footer>
+				<div class="topFoot">
+					<a href="#">Terms of Use</a>
+					<a href="#">Privacy Policy</a>
+				</div>
+				<div class="botFoot">
+					<div class="col-left">
+						Design and Developed by Syntactics Inc.
+					</div>
+					<div class="col-right">
+						Social Networking Sites
+					</div>
+				</div>
+			</footer>
+	<?php wp_footer(); ?>
+
+<!-- closing tag -->
+		</div>
+	</body>
+</html>
