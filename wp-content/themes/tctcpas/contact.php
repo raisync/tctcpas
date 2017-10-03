6bacca4729b500b5c015af571289d48c813af729
Ryan B. Abcede <ryan@syntactics.local>
2017-10-03 21:56:20 +0800

    formation

--- wp-content/themes/tctcpas/contact.php
+++ wp-content/themes/tctcpas/contact.php
@@ -1,18 +1,19 @@
 <div>
-	<div class="topCon">
+	<div class="topCon center pad30">
 		<h2>CONTACT US</h2>
-		<p>We are looking forward to hear from you</p>
+		We are looking forward to hear from you
 	</div>
-	<div class="lowCon">
-		<div class="col-left">
+	<div class="lowCon row pad30">
+		<div class="col-md-6">
 			Contact Form
 		</div>
-		<div class="col-right">
+		<div class="col-md-6">
 			<p>Thank you for taking an interest in TCT & Associates, CPAs Certified Public Accountants. Be assured that we will respond to your needs with both quality and affordable pricing. For further assistance, please contact:</p>
  
 			<p>Address: San Pedro St., Zone 6, Capisnon, Kauswagan, Cagayan de Oro City, Philippines
 			Office Phone: +63 917 707 2037
 			Email: melanie@tctcpas.com</p>
 		</div>
+		<div class="clear"></div>
 	</div>
 </div>
--- wp-content/themes/tctcpas/partners.php
+++ wp-content/themes/tctcpas/partners.php
@@ -1,23 +1,23 @@
 <div>
-	<div class="topPart">
-		<div class="col-left">
-			<h2>Become 100% Aware of Your</h2>
-			<p>Business Financial Performance</p>
+	<div class="topPart row pad30">
+		<div class="col-md-6 center">
+			<h2>Become 100% Aware of Your<br>
+			Business Financial Performance</h2>
 		</div>
-		<div class="col-right">
-			<button>CONTACT US NOW!</button>
+		<div class="col-md-6 btnContact">
+			<button class="btn btn-success btn-lg center-block">CONTACT US NOW!</button>
 		</div>
 	</div>
-					<br class="clear" />
-	<div class="lowPart">
+	<br class="clear" />
+	<div class="lowPart row pad30">
 		<h2>Our Partners</h2>
-		<div class="col-left center">
+		<div class="col-md-6 center">
 			<img src="">
 			<strong>RAYMUNDO G. TALIMIO, JR.</strong>
 			<br>Managing Partner<br>
 			<img src=""> ray_cpa
 		</div>
-		<div class="col-right center">
+		<div class="col-md-6 center">
 			<img src="">
 			<strong>MELANIE ROSALINDA P. CARAGOS-TALIMIO</strong>
 			<br>Partner<br>
